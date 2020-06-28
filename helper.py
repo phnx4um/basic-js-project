13bc2a09e247f16d5cbabe196e5feaa0f10fc44e
Nitin Verma <phnx4um@gmail.com>
2020-06-29 02:44:11 +0530

    bfs implementation

---
+++ helper.py
@@ -0,0 +1,41 @@
+
+# node keep tracks of a state, the parent of the state(the previus state) 
+# and the action we took to transition from the previous state to current state
+class Node():
+    def __init__(self, state, parent, action):
+        # state in this problem is the current position (x,y) coordiate in the maze 
+        self.state = state
+        self.parent = parent
+        self.action = action
+
+# this frontier is used for implementing DFS
+class StackFrontier():
+    def __init__(self):
+        self.frontier = []
+
+    def add(self, node):
+        self.frontier.append(node)
+
+    def contains_state(self, state):
+        return any(node.state == state for node in self.frontier)
+
+    def empty(self):
+        return len(self.frontier) == 0
+
+    def remove(self):
+        if self.empty():
+            raise Exception("empty frontier")
+        else:
+            node = self.frontier[-1]
+            self.frontier = self.frontier[:-1]
+            return node
+
+# this frontier is used for implementing BFS
+class QueueFrontier(StackFrontier):
+    def remove(self):
+        if self.empty():
+            raise Exception("empty frontier")
+        else:
+            node = self.frontier[0]
+            self.frontier = self.frontier[1:]
+            return node
---
+++ search-bfs.py
@@ -0,0 +1,77 @@
+# dummy data for now.. 
+# this will be replaced by the data from the javascript application
+from helper import QueueFrontier
+from helper import Node
+
+class Maze:
+    
+    def __init__(self):
+        self.maze_width = 4
+        self.maze_height = 4
+        self.start = (4,1)
+        self.goal = (1,4)
+        self.visited = []
+        self.walls = [[1,0,0,0], 
+                      [0,1,0,1],
+                      [0,1,0,0],
+                      [0,0,0,1]]
+        self.frontier = QueueFrontier()
+
+        start_node = Node(self.start, None, None)
+        self.frontier.add(start_node)
+
+    def bfs(self):
+        if self.frontier.empty():
+            return 0
+        while(not self.frontier.empty()):
+            node = self.frontier.remove()
+            if node.state == self.goal:
+                return node # solution found
+
+            self.visited.append(node.state) 
+            print(f'NODE: {node.state}')
+            # expand nodes
+            neighbours = self.find_neighbours(node)
+            for neighbour in neighbours:
+                # if the neighbour node is not already in the frontier
+                # or in the visited list
+                # then only add the node in the frontier....
+                if neighbour.state not in self.visited and not self.frontier.contains_state(neighbour):
+                    print(f'neighbour: {neighbour.state}')
+                    self.frontier.add(neighbour)
+
+    def find_neighbours(self, n):
+        neighbour_list = []
+        row,column = n.state
+        # not looking at the diagonal neighbours
+        neighbour_options = [( (row,column+1),"RIGHT" ),    # neighbour right
+                             ( (row-1,column),"TOP"   ),    # neighbour top
+                             ( (row,column-1),"LEFT"  ),    # neighbour left
+                             ( (row+1,column),"BOTTOM")]    # neighbiut bottom
+
+        for option in neighbour_options:
+            new_pos, action = option
+            if (
+                (new_pos[0] <= self.maze_height and new_pos[1] <= self.maze_width) and 
+                (new_pos[0] > 0 and new_pos[1] > 0) and 
+                not self.walls[new_pos[0]-1][new_pos[1]-1]
+               ): 
+                neighbour_list.append(Node(new_pos, n, action))  #neighbour right
+    
+        return neighbour_list
+
+    def solution(self):
+        path = []
+        sol_node = self.bfs()
+        node = sol_node
+        # make the BFS path
+        while(node is not None):
+            path.append(node)
+            node = node.parent
+        # print the resutl
+        path.reverse()
+        for node in path:
+            print(f'{node.action} {node.state}', end=" ")  
+
+maze = Maze()
+maze.solution()
